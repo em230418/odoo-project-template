4a567f3a4110f58ac912401cf59743cb92c64eb9
Eugene Molotov <molotov@it-projects.info>
2021-09-14 11:24:55 +0500

    Подготовил настройки для saas

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -xe
+mkdir src
+cd src
+git clone git@github.com:em230418/server-auth.git -b 14.0 --single-branch
+git clone git@github.com:em230418/saas-addons.git -b 14.0 --single-branch
