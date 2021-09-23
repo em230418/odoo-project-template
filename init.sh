df36cc8da652d829f12e3855da45557a2421416b
Eugene Molotov <molotov@it-projects.info>
2021-09-23 17:40:05 +0500

    Добавил репозиторий с аддонами для тестов

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ mkdir src
 cd src
 git clone git@github.com:em230418/server-auth.git -b 14.0 --single-branch
 git clone git@github.com:em230418/saas-addons.git -b 14.0 --single-branch
+git clone git@github.com:em230418/saas-test-addons.git -b 14.0 --single-branch
