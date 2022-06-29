a9462952ccad00d4d29dbd08ba8d6d997999f823
Eugene Molotov <molotov@it-projects.info>
2022-06-29 17:26:30 +0500

    saved

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker-compose run --rm \
+               odoo odoo \
+               -d test1 \
+               -i epiklah_crm \
+               --log-handler=odoo.addons.epiklah_crm:DEBUG \
+               --stop-after-init --test-enable $@
